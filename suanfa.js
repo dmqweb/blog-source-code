ebb109af3705580627a8a164d1f6960a5f867d21
dumingqing <dumingqing@xiaomi.com>
2024-07-23 15:54:40 +0800

    'feat:添加操作系统'

--- suanfa.js
+++ suanfa.js
@@ -1,67 +1,32 @@
 /**
- * 手写一个axios
+ * new关键字
  */
-
-/**
- * 手写Promise
- */
-
-/**
- * 手写new函数
- */
-function Example(name) {
-    if(!new.target) throw new TypeError("该函数应该使用new来调用");
-    this.name = name
+function myNew() {
+    let obj = {};
+    let Constructor = [].shift.call(arguments);
+    Object.setPrototypeOf(obj,Constructor.prototype);
+    let res = Constructor.apply(obj,arguments);
+    return typeof res === 'object' ? res || obj : obj; //res || obj考虑了构造函数显示返回null
 }
-Object.definePropertie(Example.prototype,"init",{
-    enumerable:false,
-    value: function () {
-        'use strict';
-        if(new.target) throw new Error("init函数不能使用new来调用");
-        let fn = function () {
-            console.log(this.name);
-        }
-        fn.call(this);
-    }
-})
 /**
- * 把一个函数进行柯里化
+ * instanceof
  */
-// fn(a,b,c,d) => fn(a)(b)(c)(d)
-function curried(fn) {
-    return function curried(...args) {
-        //递归到终点，执行函数
-        if(args.length >= fn.length){
-            return fn.apply(this,args);
-        }else{
-            // 递归返回函数，将之前传入的参数和之后传入的参数合并
-            return function (...args2) {
-                return curried.apply(this,args.concat(args2));
-            }
+function myInstanceOf(left,right) {
+    //判断left是不是right的子类，遍历left的原型链，看是否有right
+    if(typeof left !== "object") throw new TypeError("Left-hand side of myInstanceOf is not an object");
+    if(typeof right !== "object") throw new TypeError("Right-hand side of myInstanceOf is not an object");
+    while (Object.getPrototypeOf(left)) {
+        left = Object.getPrototypeOf(left);
+        if(left === Object.getPrototypeOf(right)){
+            return true;
         }
     }
+    return false;
 }
 /**
- * 状态模式,状态模式允许一个对象在其内部改变的时候去改变它的行为，对象看起来修改了它的类
+ * 实现Object.create
  */
-class State{
-    constructor(state){
-        this.state=state;
-    }
-    handle(context){
-        // State的handle方法，改变灯光状态
-        console.log(`this is ${this.state} light`);
-        context.setState(this);
-    }
-}
-class Context{
-    constructor(){
-        this.state = null;
-    }
-    getState(){
-        return this.state
-    }
-    setState(){
-        this.state = state;
-    }
-}
+// 参数一：proto 二：属性对象
+// Object.myCreate = function () {
+    
+// }
